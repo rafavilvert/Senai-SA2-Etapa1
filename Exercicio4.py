324afccc1c0b4e3adb052ca1e7cc15701f7c45ea
Rafael <rafavilvert@hotmai.com>
2021-06-16 17:00:53 -0300

    Exercício 4 concluído

---
+++ Exercicio4.py
@@ -0,0 +1,8 @@
+# 4. Crie uma função para desenhar uma linha, usando o caractere '_' (underline). O tamanho da linha deve
+# ser definido na chamada da função.
+
+def linha(b):
+    a = "_"
+    print(a*b)
+
+linha(10)
