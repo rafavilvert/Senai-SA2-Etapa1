904a7e4418d778b5ad8fb35f97feada487c2ca2f
Rafael <rafavilvert@hotmai.com>
2021-06-16 17:03:43 -0300

    Exercício 6 concluído

---
+++ Exercicio6.py
@@ -0,0 +1,13 @@
+# 6. Crie um programa que converta horas em segundos, 
+# conforme o valor que o usuário informar quan-
+# do solicitado a ele.
+
+segundos = float(input("Digite a hora: "))
+print(segundos, "São", segundos*3600, "segundos")
+
+print("============================================================")
+
+# 7. Altere o código a seguir, para o menor número de linhas possível,
+# mantendo o mesmo resultado:
+
+print("    *\n   * *\n  *   *\n *     *\n *** ***\n   * *\n   * *\n  *****")
