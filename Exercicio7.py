c7a0c26672bf54bb38ff645968b44b4f29f6e39e
Rafael <rafavilvert@hotmai.com>
2021-07-01 22:14:16 -0300

    Adicionando nova formas de resolver Exercício7

--- Exercicio7.py
+++ Exercicio7.py
@@ -2,3 +2,9 @@
 # mantendo o mesmo resultado:
 
 print("    *\n   * *\n  *   *\n *     *\n *** ***\n   * *\n   * *\n  *****")
+
+#Outra maneira de fazer
+lista = ['    *','   * *','  *   *',' *** ***', '   * *', '   * *', '  *****']  
+
+for i in lista:
+    print(i) 
